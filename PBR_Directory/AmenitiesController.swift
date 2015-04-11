37e51beed64da2c10c39435a60427adfd6b18354
tll-aww-app <owner@SSE-00-00-MBP-O-3.local>
2015-04-11 00:28:15 -0500

    Finished Amenities & Activities. Started on Contact.

---
+++ PBR_Directory/AmenitiesController.swift
@@ -0,0 +1,26 @@
+//
+//  AmenitiesController.swift
+//  PBR_Directory
+//
+//  Created by Owner on 4/10/15.
+//  Copyright (c) 2015 AlexWatts. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class AmenitiesController: UIViewController, UIScrollViewDelegate{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ PBR_Directory/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  PBR_Directory
+//
+//  Created by Owner on 3/29/15.
+//  Copyright (c) 2015 AlexWatts. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+}
+
